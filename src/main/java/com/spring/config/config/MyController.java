a795975f5d66fbf6985526fcc9338dd00fa57840
Sachin <sachinkmr375@gmail.com>
2020-08-18 22:21:33 +0530

    accessing property using @value

--- src/main/java/com/spring/config/config/MyController.java
+++ src/main/java/com/spring/config/config/MyController.java
@@ -1,2 +1,18 @@
-package com.spring.config.config;public class MyController {
+package com.spring.config.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @Value("${myCustomMessage}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
 }
---
+++ src/test/java/com/spring/config/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.config.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
